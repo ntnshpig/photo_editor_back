df49ebdd2bb9452a79f3af4f1557994677d090ac
Natsu67 <dableboble@gmail.com>
2022-01-08 14:31:44 +0200

    samesite none

--- config/session.php
+++ config/session.php
@@ -169,7 +169,7 @@ return [
     */
 
     //'secure' => env('SESSION_SECURE_COOKIE'),
-    'secure' => false,
+    'secure' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -197,6 +197,6 @@ return [
     |
     */
 
-    'same_site' => 'lax',
+    'same_site' => 'none',
 
 ];
